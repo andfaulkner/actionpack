1f9390fa091a13379aefc895e50e8fa064c90e37
andfaulkner <andfaulkner@gmail.com>
2015-07-04 02:55:14 -0400

    first commit

---
+++ actionpack.js
@@ -0,0 +1,40 @@
+var actionPack = function() {
+    var isArray = function(obj) {
+        return (obj && typeof obj === 'object' && obj.constructor == Array) || 
+            false;
+    }
+    return({
+        fnArr: [],
+        addFnEnd: function(fn, argArr, thisVal) {
+            var len;
+            //Add nothing if it didn't receive a function
+            if (! (typeof fn === "function")) return false;
+            this.fnArr.push({ func: fn });
+            len = this.fnArr.length - 1;
+            if (typeof argArr !== "undefined") this.fnArr[len].args = argArr;
+            this.fnArr[len].thisVal = thisVal || fn;
+            return this.fnArr;
+        },
+
+        runAll: function(isKeep) {
+            this.fnArr.forEach(function(fn){
+                fn.thisVal = fn.thisVal || fn.func;
+                if (!fn.args) {
+                    fn['func'].call(fn.thisVal);
+                } else if (isArray(fn.args)) { 
+                    fn['func'].apply(fn.thisVal, fn.args);
+                    return;
+                } else {
+                    fn['func'].call(fn.thisVal, fn.args);
+                }
+                return;
+            });
+            if (!isKeep) this.fnArr = [];
+        },
+        
+        wipeAll: function() {
+            this.fnArr = [];
+        },
+
+    });
+};
